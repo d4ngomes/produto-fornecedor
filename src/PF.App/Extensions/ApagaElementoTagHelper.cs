5ccd234abaea6e8da4714dedad8f60fe289233f2
Daniel Gomes <danielgomesjr@gmail.com>
2021-01-09 01:06:04 -0300

    Tag Helper Customizada

---
+++ src/PF.App/Extensions/ApagaElementoTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace PF.App.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-action")]
+    public class ApagaElementoTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApagaElementoTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-action")]
+        public string ActionName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
+
+            if (ActionName.Contains(action)) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
